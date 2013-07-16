1e89a31363bcb7d107a3ec24edd5a8eabe84cb9e
David Rae <david.rae@vmsuk.com>
2013-07-16 13:51:29 +0100

    completed the wordcount.py task

--- wordcount.py
+++ wordcount.py
@@ -45,11 +45,67 @@ import sys
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
+def word_count_dict(filename):
+  """Returns a word/count dict for this filename."""
+  word_count = {}
+
+  f = open(filename, 'r')
+  for line in f:
+    
+    words = line.split()
+    
+    for word in words:
+      
+      word = word.lower()
+
+      if word in word_count:
+        word_count[word] = word_count[word] + 1
+      
+      else:
+        word_count[word] = 1
+  
+  f.close()
+  
+  return word_count
+
+def print_words(filename):
+
+  """Prints one per line '<word> <count>' sorted by word for the given file."""
+
+  word_count = word_count_dict(filename)
+
+  for key in sorted(word_count.keys()):
+    print key, word_count[key]
+
+def print_top(filename):
+  """Prints the top count listing for the given file."""
+
+  word_count = word_count_dict(filename)
+
+  
+
+  items = sorted(word_count.items(), key=get_count)
+
+  # Print the first 20
+  for item in items[-20:]:
+    print item[0], item[1]
+
+
+
+def get_count(word_count_tuple):
+  """Returns the count from a dict word/count tuple  -- used for custom sort."""
+  return word_count_tuple[1]
+
+  
+
 ###
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
+
+
+
   if len(sys.argv) != 3:
     print 'usage: ./wordcount.py {--count | --topcount} file'
     sys.exit(1)
